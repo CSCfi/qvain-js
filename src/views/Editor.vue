5609d45d78dfa1087487b73c7a0688552628d9ff
Juhapekka Piiroinen <juhapekka.piiroinen@csc.fi>
2019-07-11 12:49:39 +0300

    CSCQVAIN-273: Added help text to new dataset. (#63)
    There is now a text what to select when a new dataset is created and user does not have the data yet.

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -155,6 +155,10 @@
 				<li>In Fairdata IDA (you want to select files from IDA): "Select IDA files"</li>
 				<li>Somewhere else (you want to link files from remote location): "Link Remote Resources"</li>
 				</ul></p>
+
+				<p>
+					You can also make descriptions without any files. In that case, please select either one. The selection cannot be re-done, so if you are not sure whether you'll add files later, select the one you think you'll need in the future.
+				</p>
 			</div>
 		</div>
 		<div v-else>
