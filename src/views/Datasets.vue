9cf7174c0a45134d3eaea00d9f02362066af6025
Juhapekka Piiroinen <juhapekka.piiroinen@csc.fi>
2019-07-15 17:48:12 +0300

    CSCQVAIN-279: remove alerts for successful events. (#86)
    There is no need to show an alert for the end user when something is a success.

--- src/views/Datasets.vue
+++ src/views/Datasets.vue
@@ -486,8 +486,6 @@ export default {
 			this.error = null
 			try {
 				await apiClient.delete("/datasets/" + this.itemToBeDeleted.id)
-				this.$root.showAlert("Successfully deleted dataset", "success")
-
 				await this.fetchDataset()
 				this.$refs.datasetTable.refresh()
 			} catch (e) {
