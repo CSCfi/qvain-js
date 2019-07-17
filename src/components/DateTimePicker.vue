377c912c1967055c6b1e73c8d7f7e5d7a71d69b9
Juhapekka Piiroinen <juhapekka.piiroinen@csc.fi>
2019-07-17 19:38:39 +0300

    CSCQVAIN-288, 308, 297: rewrite of date, time and date time pickers (#93)
    Fixed CSCQVAIN-288, CSCQVAIN-308, CSCQVAIN-297.
    
    This commit adds new DatePicker, TimePicker, DateTimePicker components. The dependency to external library for DatePicker use has been removed temporarily.
    
    This commit fixes the possible data corruption issue with data fields where date-time was processed. Earlier it was not possible to have such data in user interface, which might have lead to data corruption or the minimum user might have entered invalid time data with those entries, as those supported only date.
    
    The user interface helpers for other than free-form text input will be implemented in the near future.

---
+++ src/components/DateTimePicker.vue
@@ -0,0 +1,165 @@
+<!-- ADD_LICENSE_HEADER -->
+<template>
+	<b-container class="datetimepicker">
+		<b-row>
+			<b-col>
+				<b-form>
+					<b-form-group
+						:label="title"
+						:description="description">
+						<datepicker
+							v-if="format === 'date-time' || format === 'date'"
+							:format="format"
+							v-model="date">
+						</datepicker>
+						<timepicker
+							v-if="format === 'date-time' || format === 'time'"
+							:format="format"
+							v-model="time">
+						</timepicker>
+						<timezonepicker
+							v-if="format === 'date-time' || format === 'time'"
+							:format="format"
+							:timezonedate="date"
+							v-model="timezone">
+						</timezonepicker>
+					</b-form-group>
+				</b-form>
+			</b-col>
+		</b-row>
+	</b-container>
+</template>
+
+<style lang="scss">
+.datetimepicker.container {
+	margin: 0em;
+	padding: 0em;
+	.row {
+		margin: 0em;
+		padding: 0em;
+		padding-left: 0.5em;
+		.col, .col-sm-2 {
+			margin: 0em;
+			padding: 0em;
+		}
+	}
+}
+.datetimepicker.container * .input-group {
+	margin-top: 0.5em;
+}
+.datetimepicker.container * .input-group:last-child {
+	margin-top: 0.25em;
+	padding-left: 1em;
+}
+</style>
+
+<script>
+import DatePicker from "@/components/DatePicker.vue"
+import TimePicker from "@/components/TimePicker.vue"
+import TimeZonePicker from "@/components/TimeZonePicker.vue"
+
+export default {
+	name: 'datetime-picker',
+	components: {
+		"datepicker": DatePicker,
+		"timepicker": TimePicker,
+		"timezonepicker": TimeZonePicker,
+	},
+	props: {
+		format: String,
+		value: String,
+		title: String,
+		description: String,
+	},
+	data() {
+		return {
+			internalValue: null,
+			initialValue: null,
+			date: null,
+			time: null,
+			timezone: null,
+			isInitializing: true,
+		}
+	},
+
+	methods: {
+		toExternalDateFormat(internalFormat) {
+			if (!internalFormat) { return internalFormat }
+			const [date, time] = internalFormat.split("T")
+			return date
+		},
+		toExternalTimeFormat(internalFormat) {
+			if (!internalFormat) { return internalFormat }
+			const [date, time] = internalFormat.split("T")
+			return time
+		},
+		fromExternalDateFormat(externalFormat) {
+			if (!externalFormat) { return externalFormat }
+			const [year, month, day] = externalFormat.split("-")
+			return day + "." + month + "." + year
+		},
+		updateValue() {
+			if (this.isInitializing) { return }
+			let timeValue = this.time
+			if (!timeValue) {
+				timeValue = "00:00:00"
+			}
+			let dateValue = this.date
+			if (!dateValue) {
+				dateValue = "0000-00-00"
+			}
+			let timeZoneValue = this.timezone
+			if (!this.timezone) {
+				timeZoneValue = "00:00"
+			}
+			if (timeZoneValue[0] !== "-" && timeZoneValue[0] !== "+") {
+				timeZoneValue = "-" + timeZoneValue
+			}
+			this.internalValue = dateValue + "T" + timeValue + timeZoneValue
+			if (this.format === "date-time") {
+				this.$emit('input', this.internalValue)
+			} else if (this.format === "date") {
+				this.$emit('input', this.toExternalDateFormat(this.internalValue))
+			} else if (this.format === "time") {
+				this.$emit('input', this.toExternalTimeFormat(this.internalValue))
+			} else {
+				console.log("Unhandled data format (" + this.format + ") for DateTimePicker")
+			}
+		}
+	},
+	created() {
+		this.isInitializing = true
+		this.internalValue = this.value
+		this.initialValue = this.value
+		if (this.internalValue) {
+			const timeValue = this.internalValue.split("T")
+			const timeValueWithTimezone = timeValue[1]
+			let timezonePrefix = "-"
+			if (timeValueWithTimezone) {
+				if (timeValueWithTimezone.indexOf("+") > 0) {
+					timezonePrefix = "+"
+				}
+				const timeAndTimezone = timeValueWithTimezone.split(timezonePrefix)
+				this.timezone = timezonePrefix + timeAndTimezone[1]
+				this.time = timeAndTimezone[0]
+			}
+			this.date = timeValue[0]
+		}
+		this.isInitializing = false
+	},
+	watch: {
+		time() {
+			if (this.isInitializing) { return }
+			this.updateValue()
+		},
+		date() {
+			if (this.isInitializing) { return }
+			this.updateValue()
+		},
+		timezone() {
+			if (this.isInitializing) { return }
+			this.updateValue()
+		},
+	},
+}
+</script>
---
+++ src/components/TimePicker.vue
@@ -0,0 +1,96 @@
+<!-- ADD_LICENSE_HEADER -->
+<template>
+	<div class="input-group">
+		<div class="input-group-prepend">
+			<span class="input-group-text">
+				Time
+			</span>
+		</div>
+		<input
+			:value="timeString"
+			@input="validate"
+			@change="submitChange"
+			placeholder="hh:mm:ss"
+			class="form-control" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'timepicker',
+	model: {
+		prop: 'value',
+		event: 'input'
+	},
+	props: {
+		format: String,
+		value: String,
+	},
+	data() {
+		return {
+			internalValue: null,
+			initialValue: null,
+			isInitializing: true,
+			inputRegexp: /^[\d:]+$/,
+			timeRegexp: /^((0[0-9]|1\d|2[0-3]|[0-9])|((0[0-9]|1\d|2[0-3]|[0-9]):){1}(([0-5][0-9]|[0-9]):?){1,2})$/
+		}
+	},
+	computed: {
+		timeString: {
+			get() {
+				return this.internalValue
+			},
+			set(newValue) {
+				if (this.isInitializing) { return }
+				if (this.timeRegexp.test(newValue)) {
+					this.internalValue = newValue
+				}
+			}
+		},
+	},
+	methods: {
+		submitChange(event) {
+			if (this.isInitializing) { return }
+			let newValue = event.target.value
+			if (!newValue || newValue === '') {
+				this.internalValue = newValue
+				this.initialValue = newValue
+				this.$emit('input', newValue)
+			} else if (this.timeRegexp.test(newValue)) {
+				let hours = newValue.split(":")
+				const parts = hours.length
+				for(var i=0; i<hours.length; i++) {
+					hours[i] = hours[i].padStart(2, '0')
+				}
+				for (var i=0; i<3-parts; i++) {
+					hours.push("00")
+				}
+				newValue = hours.join(":")
+				this.internalValue = newValue
+				this.initialValue = newValue
+				this.$emit('input', newValue)
+			} else {
+				this.internalValue = this.initialValue
+				this.$emit('input', this.internalValue)
+			}
+		},
+		validate(event) {
+			if (this.isInitializing) { return }
+			const newValue = event.target.value
+			if (this.timeRegexp.test(newValue)) {
+				this.internalValue = newValue
+				this.initialValue = this.internalValue
+			} else if (!this.inputRegexp.test(newValue) && newValue !== '') {
+				this.internalValue = this.initialValue
+				this.$forceUpdate();
+			}
+		},
+	},
+	created() {
+		this.isInitializing = true
+		this.internalValue = this.value
+		this.initialValue = this.value
+		this.isInitializing = false
+	}
+}
+</script>
