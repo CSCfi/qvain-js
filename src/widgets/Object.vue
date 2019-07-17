12795f08400862dcaa688653ed8366b6a355088e
Juhapekka Piiroinen <juhapekka.piiroinen@csc.fi>
2019-07-17 20:17:55 +0300

    v0.10.1-rc3 - Release Candidate 3

--- src/widgets/Object.vue
+++ src/widgets/Object.vue
@@ -1,4 +1,18 @@
-<!-- ADD_LICENSE_HEADER -->
+<!--
+This file is part of Qvain -project.
+
+Author(s):
+	Juhapekka Piiroinen <jp@1337.fi>
+	Wouter Van Hemel <wouter.van.hemel@helsinki.fi>
+	Kauhia <Kauhia@users.noreply.github.com>
+	Eemeli Kouhia <eemeli.kouhia@gofore.com>
+
+License: GPLv3
+
+See LICENSE file for more information.
+Copyright (C) 2019 Ministry of Culture and Education, Finland.
+All Rights Reserved.
+-->
 <template>
 	<div :id="property + '_object'" :style="listItemStyle(depth)">
 		<header v-if="uiTitle">
