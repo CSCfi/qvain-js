8b6f392aed79e7fa43f8f6047b1de811bd6dd5f4
Jori Niemi <3295718+tahme@users.noreply.github.com>
2019-08-20 14:22:48 +0300

    revert deletebutton

--- src/partials/DeleteButton.vue
+++ src/partials/DeleteButton.vue
@@ -1,21 +1,12 @@
 <!-- ADD_LICENSE_HEADER -->
 <template functional>
-	<span
-		class="delete-button"
-		:class="{pointer: !props.disabled, disabled: props.disabled}"
-		variant="link"
-		@click="(e)=>props.disabled ? null : listeners.click(e)"
-	>
-		<font-awesome-icon
-			icon="trash"
-			fixed-width
-			class="icon"
-		/>
+	<span class="pointer delete-button" variant="link" v-on:click="listeners.click">
+		<font-awesome-icon icon="trash" fixed-width class="icon" />
 	</span>
 </template>
 
 <style lang="scss" scoped>
-.delete-button:not(.disabled) {
+.delete-button {
 	padding: 4px 4px;
 	border-radius: 4px;
 
@@ -28,19 +19,10 @@
 		background: rgba(0,0,0,0.1);
 	}
 }
-
-.disabled {
-	color: #ccc;
-}
 </style>
 
 <script>
 export default {
 	name: 'delete-button',
-	props: {
-		disabled: {
-			type: Boolean,
-		},
-	},
 }
 </script>
