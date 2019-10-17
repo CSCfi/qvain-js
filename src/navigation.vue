9aa14ed8c83961fd69cfe607a1d6ceca9cf090a0
Jori Niemi <3295718+tahme@users.noreply.github.com>
2019-10-17 11:52:04 +0300

    CSCQVAIN-391: Use <li> elements in navigation lists (#177)

--- src/navigation.vue
+++ src/navigation.vue
@@ -21,15 +21,17 @@
 			is-nav
 		>
 			<b-navbar-nav>
-				<b-button
-					variant="primary"
+				<b-nav-item
+					link-classes="btn btn-primary"
 					target="_blank"
 					rel="noopener noreferrer"
 					href="https://www.fairdata.fi/en/qvain/qvain-user-guide/"
 				>
-					<font-awesome-icon icon="info" />&nbsp;
-					User Guide
-				</b-button>
+					<font-awesome-icon
+						icon="info"
+						class="mr-2"
+					/>User Guide
+				</b-nav-item>
 			</b-navbar-nav>
 
 			<!-- right-aligned items -->
@@ -48,43 +50,43 @@
 					/>
 				</b-nav-text>
 
-				<b-button-group v-else>
-					<b-button
+				<template v-else>
+					<b-nav-item
 						v-if="$auth.loggedIn"
 						id="usermenu_userinfo"
-						variant="primary"
+						link-classes="btn btn-primary"
 						to="/userinfo"
 					>
 						<font-awesome-icon
 							icon="user"
-							class="text-light mr-2"
-							fixed-width
-						/> <a id="usermenu_fullname">{{ $auth.user.name }}</a>
-					</b-button>
+							class="mr-2"
+						/><span id="usermenu_fullname">{{ $auth.user.name }}</span>
+					</b-nav-item>
 
-					<b-button
+					<b-nav-item
 						v-if="$auth.loggedIn || $auth.getLoginError()"
 						id="usermenu_signout"
-						variant="primary"
+						link-classes="btn btn-primary"
 						@click="logout()"
 					>
-						<font-awesome-icon icon="sign-out-alt" />
-						&nbsp;
-						Sign out
-					</b-button>
-					<b-button
+						<font-awesome-icon
+							icon="sign-out-alt"
+							class="mr-2"
+						/>Sign out
+					</b-nav-item>
+					<b-nav-item
 						v-else
 						id="usermenu_login"
 						key="user-login"
-						class="user-nav"
-						variant="primary"
+						link-classes="user-nav btn btn-primary"
 						:href="$auth.loginUrl"
 					>
-						<font-awesome-icon icon="sign-in-alt" />
-						&nbsp;
-						Login
-					</b-button>
-				</b-button-group>
+						<font-awesome-icon
+							icon="sign-in-alt"
+							class="mr-2"
+						/>Login
+					</b-nav-item>
+				</template>
 			</b-navbar-nav>
 		</b-collapse>
 	</b-navbar>
@@ -93,12 +95,6 @@
 <script>
 export default {
 	name: 'Navigation',
-	data: function() {
-		return {
-		}
-	},
-	computed: {
-	},
 	methods: {
 		async logout() {
 			if (!await this.$auth.logout()) {
